aeada5d7d0c8ddd14bcbaccf01f766e5aafd4f3f
Aditya Hurkat <adityakr.hurkat.min17@itbhu.ac.in>
2021-02-17 10:54:15 +0530

    changes

---
+++ TruckX/Models/TodoItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruckX.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string surname { get; set; }
+
+
+        public string type { get; set; }
+        public string imei { get; set; }
+        public string alarmType { get; set; }
+        public DateTime alarmTime { get; set; }
+        public double latitude { get; set; }
+        public string fileList { get; set; }
+        public DateTime locationTime { get; set; }
+        public string fileName { get; set; }
+        public string data { get; set; }
+
+    }
+}
